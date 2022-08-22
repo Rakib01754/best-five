eccecad4b02828e041c21750bd6745096564501b
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-08-22 11:37:47 +0600

    remove button display function added

--- js/calculation.js
+++ js/calculation.js
@@ -1,3 +1,12 @@
+function removeButtonDisplay() {
+    const selectedListElement = document.getElementById('selected-area');
+    const selectedNumber = selectedListElement.childElementCount;
+    const removeBtn = document.getElementById('remove-btn');
+    if (selectedNumber === 0) {
+        removeBtn.classList.remove('d-none')
+    }
+}
+
 function playerSelection(player) {
     const playerParent = player.parentNode;
     const playerParentChilds = playerParent.childNodes;
@@ -9,6 +18,7 @@ function playerSelection(player) {
         alert('আপনি আপনার দলের জন্য সর্বোচ্চ ৫ জন খেলোয়াড় নির্বাচন করতে পারবেন।');
         return;
     }
+    removeButtonDisplay();
     const playersLi = document.createElement('li');
     playersLi.innerText = playerName;
     const selectedPlayer = selectedListElement.appendChild(playersLi);
