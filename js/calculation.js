03319f09b7f5e3379628977ef3b7d50dd88bfb29
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-08-22 13:07:18 +0600

    some code removed

--- js/calculation.js
+++ js/calculation.js
@@ -1,29 +1,21 @@
-function removeButtonDisplay() {
-    const selectedListElement = document.getElementById('selected-area');
-    const selectedNumber = selectedListElement.childElementCount;
-    const removeBtn = document.getElementById('remove-btn');
-    if (selectedNumber === 0) {
-        removeBtn.classList.remove('d-none')
-    }
-}
 
 function playerSelection(player) {
     const playerParent = player.parentNode;
     const playerParentChilds = playerParent.childNodes;
     const playerNameField = playerParentChilds[1];
     const playerName = playerNameField.innerText;
-    const selectedListElement = document.getElementById('selected-area');
-    const selectedNumber = selectedListElement.childElementCount;
+    const selectedListArea = document.getElementById('selected-area');
+    const selectedNumber = selectedListArea.childElementCount;
     if (selectedNumber === 5) {
         alert('আপনি আপনার দলের জন্য সর্বোচ্চ ৫ জন খেলোয়াড় নির্বাচন করতে পারবেন।');
         return;
     }
-    removeButtonDisplay();
     const playersLi = document.createElement('li');
     playersLi.innerText = playerName;
-    const selectedPlayer = selectedListElement.appendChild(playersLi);
+    const selectedPlayer = selectedListArea.appendChild(playersLi);
     player.setAttribute('disabled', "")
     player.innerText = 'Selected';
     return selectedPlayer;
-
 }
+
+
